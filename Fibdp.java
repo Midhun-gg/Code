61b3cd24f699c5b1e33cf7bf5f0f94b1f9bb6440
Midhun-gg <midhungullapalli9@gmail.com>
2026-05-25 00:52:49 +0530

    unnecessary

--- Fibdp.java
+++ Fibdp.java
@@ -3,7 +3,10 @@ public class Fibdp {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        if(n==0)return;
+        if(n==0){
+            sc.close();
+            return;
+        }
         int[] fib=new int[n];
         if(n>1)fib[1]=1;
         for(int i=2;i<n;i++){
